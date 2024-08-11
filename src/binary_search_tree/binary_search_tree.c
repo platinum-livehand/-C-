80dce02af8842d6d9517c9490b61f5ae4a0edf87
qiyang <qiyang@remote.com>
2024-08-11 17:18:40 +0800

    add 二叉排序树

---
+++ src/binary_search_tree/binary_search_tree.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TreeNode
+{
+    int data;
+    struct TreeNode* lchild;
+    struct TreeNode* rchild;
+} TreeNode;
+
+void bstInsert(TreeNode** node, int data)
+{
+    if (*node == NULL)
+    {
+        *node = (TreeNode*)malloc(sizeof(TreeNode));
+        (*node)->data = data;
+        (*node)->lchild = NULL;
+        (*node)->rchild = NULL;
+    }
+    else
+    {
+        if ((*node)->data < data)
+        {
+            bstInsert(&((*node)->rchild), data);
+        }
+        else if ((*node)->data > data)
+        {
+            bstInsert(&((*node)->lchild), data);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
+
+TreeNode* bstSearch(TreeNode* root, int data)
+{
+    if (root ==  NULL)
+    {
+        return NULL;
+    }
+    
+    if (root->data == data)
+    {
+        return root;
+    }
+    else if (root->data < data)
+    {
+        return bstSearch(root->rchild, data);
+    }
+    else
+    {
+        return bstSearch(root->lchild, data);
+    }
+}
+
+void preOrder(TreeNode* root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    
+    printf("%d ", root->data);
+
+    preOrder(root->lchild);
+    preOrder(root->rchild);
+}
+
+int main(int argc, char* argv[])
+{
+    TreeNode* root = NULL;
+    
+    bstInsert(&root, 50);
+    bstInsert(&root, 30);
+    bstInsert(&root, 70);
+    bstInsert(&root, 20);
+    bstInsert(&root, 40);
+    bstInsert(&root, 60);
+    bstInsert(&root, 80);
+
+    printf("Pre-order traversal: ");
+    preOrder(root);
+    printf("\n");
+
+    TreeNode* searchNode = bstSearch(root, 70);
+    if (searchNode != NULL)
+    {
+        printf("Found node with data: %d\n", searchNode->data);
+    }
+    else
+    {
+        printf("Node not found.\n");
+    }
+
+    return 0;
+}
