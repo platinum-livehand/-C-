de32c7c1436aefe3cfd3f30798c4007c4e32f2ef
qy <qy@vscode_remote.com>
2024-08-28 09:23:41 +0800

    rm hello_data_struct rm hello_Linux

--- src/hello_Linux/hello_Linux.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("Hello Linux\n");
-    return 0;
-}
--- src/hello_data_structure/hello_data_structure.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-    printf("Hello Data Structure\n");
-    return 0;
-}
